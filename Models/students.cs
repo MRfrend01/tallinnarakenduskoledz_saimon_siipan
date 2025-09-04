23383b1156d2257e88f86d958e20a9d8618d6805
MRfrend <Saimon.siipan@gmail.com>
2025-09-04 13:02:59 +0300

    commit

---
+++ Models/students.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tallinnarakenduskolledz.Models
+{
+    public class students
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime Enrollmentdate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+        // lisame kolm omandust õpilase ise mõtlen välja
+        public string Email { get; set; }           // Õpilase e-posti aadress
+        public string PhoneNumber { get; set; }     // Telefoninumber
+        public DateTime DateOfBirth { get; set; }   // Sünniaeg
+    }
+}
