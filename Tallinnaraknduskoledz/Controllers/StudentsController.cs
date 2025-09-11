bab09ed627e82a14cc82e2eb15190591cb00b3a5
MRfrend <Saimon.siipan@gmail.com>
2025-09-11 13:00:29 +0300

    tegime dit
    tegime edit

--- Tallinnaraknduskoledz/Controllers/StudentsController.cs
+++ Tallinnaraknduskoledz/Controllers/StudentsController.cs
@@ -73,7 +73,54 @@ namespace Tallinnarakenduskolledz.Controllers
             }
             return View(student);
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Firstname,Lastname,EnrollmentDate,DateOfBirth,Enrollments,Email,PhoneNumber")] Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(student);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Student(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
 
+        private bool Student(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
 
--- Tallinnaraknduskoledz/obj/Debug/net8.0/Tallinnarakenduskolledz.AssemblyInfo.cs
+++ Tallinnaraknduskoledz/obj/Debug/net8.0/Tallinnarakenduskolledz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tallinnarakenduskolledz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4852e6085e18d3b702c84941bdc06cfeea49aa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ede7f0221303afda68594258ca5dbff2af1858")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tallinnarakenduskolledz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tallinnarakenduskolledz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
