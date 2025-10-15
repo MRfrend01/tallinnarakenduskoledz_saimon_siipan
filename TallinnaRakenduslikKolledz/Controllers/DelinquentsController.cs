f1d6d3906556e1496e8782ff0312bb4a80711565
MRfrend <Saimon.siipan@gmail.com>
2025-10-15 11:39:57 +0300

    Add delete functionality for Delinquents
    Implemented GET and POST actions in DelinquentsController to support deleting delinquent records. Added Delete.cshtml view to confirm and process deletions.

--- TallinnaRakenduslikKolledz/Controllers/DelinquentsController.cs
+++ TallinnaRakenduslikKolledz/Controllers/DelinquentsController.cs
@@ -33,6 +33,30 @@ namespace TallinnaRakenduslikKolledz.Controllers
         {
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Delinquents == null) { return NotFound(); }
+            var delinquent = await _context.Delinquents
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.DelinquentId == id);
+            if (delinquent == null) { return NotFound(); }
+            return View(delinquent);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Delinquents == null) { return Problem("Entity set 'SchoolContext.Delinquents'  is null."); }
+            var delinquent = await _context.Delinquents.FindAsync(id);
+            if (delinquent != null)
+            {
+                _context.Delinquents.Remove(delinquent);
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 
 }
